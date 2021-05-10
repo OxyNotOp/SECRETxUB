8375fbc927f6586a43ca2354fff18edb4bf3dbc4
Your Daddy <71930942+OxyNotOp@users.noreply.github.com>
2021-05-10 12:40:23 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -250,8 +250,8 @@ def errors_handler(func):
             from .helpers.utils import _format
 
             pastelink = _format.paste_text(ftext)
-            text = "**CatUserbot Error report**\n\n"
-            link = "[here](https://t.me/catuserbot_support)"
+            text = "**SecretXub Error report**\n\n"
+            link = "[here](https://t.me/OxyXsupport)"
             text += "If you wanna you can report it"
             text += f"- just forward this message {link}.\n"
             text += "Nothing is logged except the fact of error and date\n\n"
